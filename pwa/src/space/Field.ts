c1c57065bf45a33a6231c1973fd7f1135bfd3daf
Govind Thange <gthange@yahoo.com>
2020-03-25 20:48:14 +0530

    Resolved flicker issue caused by multiple streams updating the same canvavas and erasing each other's data

--- pwa/src/space/Field.ts
+++ pwa/src/space/Field.ts
@@ -5,6 +5,7 @@ import { Battleship } from "../objects/Battleship";
 import { EnemyMotion } from "../objects/EnemyMotion";
 import { Enemy } from "../objects/Enemy";
 import { BattleshipFiring } from "../objects/BattleshipFiring";
+import { combineLatest } from "rxjs";
 
 class Field {
     width: number;
@@ -31,45 +32,42 @@ class Field {
 
         this.totalEnemies = 14;
         let enemySource = new EnemyMotion(this.width, this.height, this.totalEnemies);
-        enemySource.stream().subscribe((arr: any) => this.renderEnemies(arr));
 
         let particleSource = new ParticleMotion(this.width, this.height, this.totalStars);
-        particleSource.stream().subscribe((arr: any) => this.renderParticles(arr));
 
         let shipMotion = new BattleshipMotion(canvas);
-        shipMotion.stream().subscribe((coord: any) => this.renderShip(coord));
 
         let shipFires = new BattleshipFiring(canvas);
 
+        let game = combineLatest(
+                        enemySource.stream(),
+                        particleSource.stream(),
+                        shipMotion.stream(),
+                        (enemies: any, stars: any, shipCoordinates) => {
+                            return {enemies: enemies, stars: stars, shipCoordinates: shipCoordinates};
+                        }
+                    );
+
+        game.subscribe(
+            (scene: any) => {
+                this.renderScene(scene.enemies, scene.stars, scene.shipCoordinates);
+            }
+        )
     }
 
-    renderSpace() {
+    renderScene(enemies: any, stars: any, shipCoordinates: any) {
         this.context.fillStyle = "#000000";
         this.context.fillRect(0, 0, this.width, this.height);
-    }
-
-    renderEnemies(enemies: Array<Enemy>) {
-        this.renderSpace();
 
         enemies.forEach(
             (enemy: Enemy) => enemy.render(this.context)
         );
 
-        this.renderShip(null);
-    }
-
-    renderParticles(stars: Array<Particle>) {
-        this.renderSpace();
-
         stars.forEach(
             (star: Particle) => star.render(this.context)
         );
 
-        this.renderShip(null);
-    }
-
-    renderShip(point: any) {
-        this.ship.render(this.context, point, "up");
+        this.ship.render(this.context, shipCoordinates, "up");
     }
 }
 

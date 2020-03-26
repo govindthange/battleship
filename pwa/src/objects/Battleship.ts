dd81dcd317de07051a8f6ef69de58a05c5223f78
Govind Thange <gthange@yahoo.com>
2020-03-26 17:05:47 +0530

    Moved code streaming logic to Battleship

--- pwa/src/objects/Battleship.ts
+++ pwa/src/objects/Battleship.ts
@@ -1,11 +1,18 @@
 import { Point } from "../core/Point";
+import { fromEvent, pipe } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 
 class Battleship {
 
     point: Point;    
     width: number;
+
+    canvas: HTMLCanvasElement;
     
     constructor(canvas: HTMLCanvasElement) {
+
+        this.canvas = canvas;
+
         this.point = {
             x: canvas.width / 2,
             y: canvas.height - 20
@@ -27,6 +34,14 @@ class Battleship {
         context.lineTo(this.point.x - this.width, this.point.y);
         context.fill();
     }
+
+    public streamCoordinates() {
+        return fromEvent(this.canvas, "mousemove") // observable of all mousemove values.
+                // Transform mousemove-event-stream to coordinate-value-streams.
+               .pipe(map((event: MouseEvent) => ({x: event.clientX, y: event.clientY})))
+                // Transform coordinate-value-stream by prepending it with a starting coordinate value
+               .pipe(startWith({x: this.canvas.width/2, y: this.canvas.height/2}));
+   }
 }
 
 export { Battleship };

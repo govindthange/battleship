2693d81ef37162dba902e87854d3b3cb3b9b2e1e
Govind Thange <govindt@pune.winsoftech.com>
2020-03-29 13:35:25 +0530

    Introduced better names for constants and classes like Aircraft & Fleet (Enemy), Projectiles (Hits) etc

---
+++ pwa/src/objects/Fleet.ts
@@ -0,0 +1,73 @@
+import { Base } from "../core/Base";
+import { Util } from "../core/Util";
+import { interval } from "rxjs";
+import { scan } from "rxjs/operators";
+
+const AIRCRAFT_MAX_SPEED: number = 15;
+const AIRCRAFT_DISPATCH_RATE: number = 1500; // Prepare 1 aircraft for attack every 1500ms
+
+class Aircraft extends Base {
+
+    speed: number;
+    color: string;
+    isDestroyed: boolean;
+    fallDirection: number = 0;
+    
+    constructor(x: number, y: number, width: number, height: number, speed: number) {
+        super(null, x, y, width, height);
+        this.speed = speed;
+        this.color = "red";
+        this.isDestroyed = false;
+    }
+
+    public render(context: any, direction: string) {
+        if (this.isDestroyed) {
+            this.color = "gray";
+            this.speed = 3;
+
+            if (this.fallDirection == 0) {
+                this.fallDirection = Util.randomBoolean() ? 1 : -1;
+            }
+
+            this.x += this.fallDirection * this.speed;
+        }
+
+        super.render(context, this.color, direction);
+    }
+}
+
+class Fleet extends Base {
+    
+    constructor(canvas: HTMLCanvasElement) {
+        super(
+            canvas,
+            0,
+            0,
+            canvas.width,
+            canvas.height);
+    }
+
+    dispatchAircraft() {
+        let x = Util.random(this.width),
+            y = -25,
+            width = Util.randomRange(1, 10),
+            height = Util.randomRange(1, 10); 
+
+        let speed = AIRCRAFT_MAX_SPEED / Util.randomRange(1, 3);
+        
+        return new Aircraft(x, y, width, height, speed);
+    }
+
+    public streamCoordinates() {
+        return interval(AIRCRAFT_DISPATCH_RATE)
+        //return fromEvent(document, "keydown")
+                .pipe(scan((enemies: any) => {
+                    enemies = enemies.filter((e: any) => e.y < this.height);
+                    enemies.push(this.dispatchAircraft());
+                    return enemies;
+                }, []))
+    }
+
+}
+
+export { Aircraft, Fleet };
